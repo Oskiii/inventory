b4f1c528575e843b5ae43f8356d4daaf19645645
Oskiii <oliukku@gmail.com>
2015-10-01 22:40:56 +0300

    s

---
+++ Assets/DialoguePanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class DialoguePanel : MonoBehaviour {
+
+	public GameObject questionPanel;
+	public GameObject answerPanel;
+	public List<dialogueNode> nodeList;
+
+	[System.Serializable]
+	public class dialogueNode{
+		public Sprite speakerIcon;
+		public int dialogueNodeID;
+		public string question;
+		public string answer1;
+		public string answer2;
+		public string answer3;
+		public int nextNodeID;
+	}
+	
+
+
+	void Start(){
+
+	}
+
+}
